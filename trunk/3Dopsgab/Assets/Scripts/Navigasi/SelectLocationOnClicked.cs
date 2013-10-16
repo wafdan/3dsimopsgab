671618d8ce5342e8e5f55d2aef4ef4b7af45d33b
zorvanion@gmail.com <zorvanion@gmail.com@445212a8-dc9b-e79a-29b3-f8751ee92aca>
2013-10-16 01:06:06 +0000

    - ganti nama onclicked

---
+++ trunk/3Dopsgab/Assets/Scripts/Navigasi/SelectLocationOnClicked.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class SelectLocationOnClicked : MonoBehaviour {
+	
+    //private UnitManager unitManager;
+   //private CameraMovement cam;
+	
+   //void Start() {
+   //    GameObject unitManagerObject = GameObject.FindGameObjectWithTag("unitmanager");
+   //    unitManager = unitManagerObject.GetComponent<UnitManager>();
+   //}
+    void Clicked()
+    {
+        //Application.LoadLevel(1);
+        Application.LoadLevelAdditive("Sangatta Serang");
+
+        //camera.cullingMask = ~(1 << LayerMask.NameToLayer("Water"));
+
+        //BasicUnitMovement.isSelected = true;
+        //if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        //{
+        //    unitManager.SelectAdditionalUnit(gameObject);
+
+        //}
+        //else
+        //{
+        //    unitManager.SelectSingleUnit(gameObject);
+        //}
+    }
+}
