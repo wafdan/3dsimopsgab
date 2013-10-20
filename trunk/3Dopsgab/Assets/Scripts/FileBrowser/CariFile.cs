31447c95196473362da522c84127682c29d0bb92
fery.geps <fery.geps@445212a8-dc9b-e79a-29b3-f8751ee92aca>
2013-10-20 07:41:03 +0000

    meta nya udah sy add semua.. commit lagi

---
+++ trunk/3Dopsgab/Assets/Scripts/FileBrowser/CariFile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+ 
+public class CariFile : MonoBehaviour {
+ 
+	protected string m_textPath;
+ 
+	protected FileBrowser m_fileBrowser;
+ 
+	[SerializeField]
+	protected Texture2D	m_directoryImage,
+						m_fileImage;
+ 
+	protected void OnGUI () {
+		if (m_fileBrowser != null) {
+			m_fileBrowser.OnGUI();
+		} else {
+			OnGUIMain();
+		}
+	}
+ 
+	protected void OnGUIMain() {
+ 
+		GUILayout.BeginHorizontal();
+			GUILayout.Label("File Pendukung :", GUILayout.Width(100));
+			GUILayout.FlexibleSpace();
+			GUILayout.Label(m_textPath ?? "belum dipilih");
+			if (GUILayout.Button("...", GUILayout.ExpandWidth(false))) {
+				m_fileBrowser = new FileBrowser(new Rect(100, 100, 600, 500),"Pilih File",FileSelectedCallback);
+				m_fileBrowser.SelectionPattern = "*.*";
+				m_fileBrowser.DirectoryImage = m_directoryImage;
+				m_fileBrowser.FileImage = m_fileImage;
+			}
+		//GUI.Button()
+		GUILayout.EndHorizontal();
+	}
+ 
+	protected void FileSelectedCallback(string path) {
+		m_fileBrowser = null;
+		m_textPath = path;
+	}
+}
---
+++ trunk/3Dopsgab/Assets/Scripts/GUI/CameraSangatta.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSangatta : MonoBehaviour
+{
+	
+	private const int ScrollSpeed = 200;
+	private const int ScrollWidth = 10;
+	private const int MaxCameraHeight = 600;
+	private const int MinCameraHeight = 100;
+	private const int RotateAmount = 10;
+	private const int RotateSpeed = 100;
+	
+	void start(){
+	}
+	
+	void Update ()
+	{
+		MoveCamera ();
+		RotateCamera ();
+	}
+	
+	private void MoveCamera ()
+	{
+		if (Camera.main != null) {
+			float xpos = Input.mousePosition.x;
+			float ypos = Input.mousePosition.y;
+			Vector3 movement = Vector3.zero;
+			
+			Vector3 origin = Camera.main.transform.position;
+			Vector3 destination = origin;
+		
+			int width = Screen.width;
+			int height = Screen.height;
+		
+			//horizontal movement
+			if ((xpos >= 0 && xpos < ScrollWidth || Input.GetKey (KeyCode.A)))
+			{			
+				movement.x -= ScrollSpeed;
+				//if( origin.x >= 320f)
+				//{
+				//	origin = new Vector3(320f, origin.y, origin.z);	
+				//}
+			}	
+			
+			else if (xpos <= width && xpos > width - ScrollWidth || Input.GetKey (KeyCode.D))
+			{
+				movement.x += ScrollSpeed;
+				
+				//if( origin.x <= -310f)
+				//{
+				//	origin = new Vector3(-310f, origin.y, origin.z);	
+				//}
+			}
+		
+			//vertical camera movement
+			if (ypos >= 0 && ypos < ScrollWidth || Input.GetKey (KeyCode.S))
+			{
+				movement.z -= ScrollSpeed;
+				
+				//if( origin.z > 550f)
+				//{
+				//	origin = new Vector3( origin.x, origin.y, 550f);	
+				//}
+			}
+			else if (ypos <= height && ypos > height - ScrollWidth || Input.GetKey (KeyCode.W))
+			{
+				movement.z += ScrollSpeed;
+				
+				//if( origin.z < 200f)
+				//{
+				//	origin = new Vector3( origin.x, origin.y, 200f);	
+				//}
+				
+			}
+		
+			movement = Camera.main.transform.TransformDirection (movement);
+			movement.y = 30;
+		
+			//away from the ground
+			movement.y = ScrollSpeed * Input.GetAxis ("Mouse ScrollWheel");
+		
+			
+			if (movement != Vector3.zero)
+				destination += movement;
+		
+			if (destination.y > MaxCameraHeight)
+				destination.y = MaxCameraHeight;
+			else if (destination.y < MinCameraHeight)
+				destination.y = MinCameraHeight;
+		
+			if (origin != destination)
+				Camera.main.transform.position = Vector3.MoveTowards (origin, destination, ScrollSpeed * Time.deltaTime);
+			
+		}
+	}
+
+	private void RotateCamera ()
+	{
+		if (Camera.main != null) {
+			Vector3 origin = Camera.main.transform.eulerAngles;
+			Vector3 destination = origin;
+		
+			if (Input.GetKey (KeyCode.LeftControl) && Input.GetMouseButton (0))
+				destination.y += Input.GetAxis ("Mouse X") * RotateAmount;
+		
+			if (origin != destination)
+				Camera.main.transform.eulerAngles = Vector3.MoveTowards (origin, destination, Time.deltaTime * RotateSpeed);
+		}
+	}
+	
+	// Turn on the bit using an OR operation:
+    public void Show() {
+    Camera.main.cullingMask = 1 << LayerMask.NameToLayer("Water");
+    }
+     
+    // Turn off the bit using an AND operation with the complement of the shifted int:
+    public void Hide() {
+    camera.cullingMask = ~(1 << LayerMask.NameToLayer("Water"));
+    }
+     
+    // Toggle the bit using a XOR operation:
+    //private void Toggle() {
+    //camera.cullingMask ^= 1 << LayerMask.NameToLayer("SomeLayer");
+    //}
+}
