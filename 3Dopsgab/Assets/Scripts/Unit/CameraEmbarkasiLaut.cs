ca7ec8fd7ea2694fa7bbad8e4682823e07596cfa
joyashari@gmail.com <joyashari@gmail.com@445212a8-dc9b-e79a-29b3-f8751ee92aca>
2013-10-21 10:04:53 +0000

    embarkasi tank di laut/kapal

---
+++ 3Dopsgab/Assets/Scripts/Unit/CameraEmbarkasiLaut.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEmbarkasiLaut: MonoBehaviour {
+	
+	private Transform myTransform;
+	public float kecepatanKamera;
+	
+	public Transform myTarget;
+	
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+		StartCoroutine(GantiScene());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTransform.Translate(Vector3.forward * kecepatanKamera * Time.deltaTime);
+		
+		//myTransform.LookAt(myTarget);
+	}
+	
+	IEnumerator GantiScene()
+	{
+		yield return new WaitForSeconds(20f);
+		Debug.Log("Ganti Scene");
+	}
+}
