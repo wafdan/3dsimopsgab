a0783cba33b9fb73f7e1ffbe2913858f643f0824
joyashari@gmail.com <joyashari@gmail.com@445212a8-dc9b-e79a-29b3-f8751ee92aca>
2013-10-21 10:04:53 +0000

    embarkasi tank di laut/kapal

---
+++ trunk/3Dopsgab/Assets/Scripts/Unit/EmbarkasiTank.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmbarkasiTank : MonoBehaviour {
+	
+	public Transform[] wayPoint;
+	private Transform myTransform;
+	public Transform[] myTarget;
+	
+	public float tankSpeed = 20;
+	private bool loop = false;	
+	
+	private int currentWaypoint;
+		
+	void Awake()
+	{
+		wayPoint[0] = transform;	
+	}
+	
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 velocity = rigidbody.velocity;
+		
+		if( currentWaypoint < wayPoint.Length)
+		{
+			Vector3 target = wayPoint[currentWaypoint].position;
+			Vector3 moveDirection = target - myTransform.position;
+			
+			
+			if( moveDirection.magnitude < 1)
+			{
+				//myTransform.Rotate(0,-210,0);
+				currentWaypoint++;
+			}
+			else
+			{
+				velocity = moveDirection.normalized * tankSpeed;	
+				myTransform.LookAt(myTarget[currentWaypoint]);
+			}
+		}
+		
+		else
+		{
+			if( loop)
+			{
+				currentWaypoint = 0;	
+			}
+			else
+				velocity = Vector3.zero;
+		}
+		
+		rigidbody.velocity = velocity;		
+	}
+	
+	void OnTriggerEnter( Collider otherObject)
+	{
+		if( otherObject.transform.tag == "pintu")
+		{
+			Debug.Log("im in");	
+			StartCoroutine(DestroyTank());
+		}
+	}
+	
+	IEnumerator DestroyTank()
+	{
+		yield return new WaitForSeconds(1);
+		Destroy(myTransform.gameObject);
+	}
+}
