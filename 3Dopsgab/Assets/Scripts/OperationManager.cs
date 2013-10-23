72a58e92143d12d3cc354558319c09e8e420b6c2
zorvanion@gmail.com <zorvanion@gmail.com@445212a8-dc9b-e79a-29b3-f8751ee92aca>
2013-10-23 07:10:49 +0000

    - tukar posisi "playManager" jadi di OperationManager.cs. - Penyesuaian GUI untuk form kegiatan, biar lebih indah. - Operasi upload posisinya diganti jadi sekalian pas user tekan tombol "Submit", kalo "Batal" berarti ga jadi diupload. - Operasi delete file kegiatan udah bisa, pake tombol merah di samping nama filenya. Tapi sayang masih nge-bug yaitu si file aslinya ga kedelete, jadinya cuma "mutus hubungan" antara kegiatan dgn filenya aja.

--- 3Dopsgab/Assets/Scripts/OperationManager.cs
+++ 3Dopsgab/Assets/Scripts/OperationManager.cs
@@ -31,14 +31,178 @@ public class OperationManager : MonoBehaviour
     public static int InstanceIdx = 0;
     public static string FILE_EXT=".tfgsg";
 
-
     public static DateTime HARI_HA = new DateTime(2007, 10, 1, 00, 00, 00);
 
+    //PLAY MODE RELATED
+    OperationItem opFirstPlay; //kegiatan yg diklik-play oleh user
+    private DateTime opFirstPlayStartDate; //startTime opFirstPlay
+    
+    DateTime gameClockTime; // clock virtual
+    public static string gameClockValue; // nilai clock, dipake di GUI
+    public static float gameClockSpeed = 0.1f; // kecepatan default clock
+    private string posHariInGameClock = ""; //posisi hari berdasarkan clock
 
-    public void Start()
+    public static ArrayList nowPlayingList = new ArrayList(); // list kegiatan yg sedang aktif saat clock berjalan
+
+    void Start()
     {
-        StartCoroutine(playOperation());
+        StartCoroutine(startTheGameClock());
+        StartCoroutine(playTheKegs());
     }
+
+    private IEnumerator playTheKegs()
+    {
+        Debug.Log("playTheKegs");
+        while (true)
+        {
+            yield return null;
+            if (MenuUnit.showPlayMode)
+            {
+                for (int i = MenuUnit.curOpPlayIdx; i < OperationManager.operationList.Count; i++)//DI SINI MASALAHNYA, SOLUSINYA: SORTING BY DATE
+                {
+                    yield return null;
+                    OperationItem opItIn = (OperationItem)OperationManager.operationList[i];
+
+                    if (opItIn.satuan == PlayerPrefs.GetString("satuan"))
+                    {
+                        string pos = preProcPosHar(opItIn.posisiHari);
+
+                        DateTime opStartTime = opItIn.startTime;
+                        DateTime opDoneTime = opItIn.endTime;
+
+                        if (posHariInGameClock == pos)
+                        {
+
+                            if (gameClockTime.TimeOfDay >= opStartTime.TimeOfDay)// && gameTime < opDoneTime)
+                            {
+
+                                if (!nowPlayingList.Contains(opItIn))
+                                {
+                                    nowPlayingList.Add(opItIn);
+                                }
+                            }
+                        }
+
+                        if (gameClockTime >= opDoneTime)
+                        {
+                            if (nowPlayingList.Contains(opItIn))
+                            {
+                                nowPlayingList.Remove(opItIn);
+
+                            }
+                        }
+
+                    }
+                    if (!MenuUnit.showPlayMode) { nowPlayingList.Clear(); break; }
+                }
+            }
+
+        }
+    }
+
+    private IEnumerator startTheGameClock()
+    {
+        while (true)
+        {
+            yield return null;
+            if (MenuUnit.showPlayMode)
+            {
+
+                // opIt starting point
+                opFirstPlay = (OperationItem)OperationManager.operationList[MenuUnit.curOpPlayIdx];
+                // mulai semua kegiatan dari starting point
+                gameClockTime = opFirstPlay.startTime; //timeToIncrement
+                //DateTime lastST = DateTime.Parse(opFirstPlay.startTime); //caching hari "kemarin"
+                opFirstPlayStartDate = opFirstPlay.startTime;//startTime
+                //DateTime eTim = opFirstPlay.startTime.Add(opFirstPlay.duration); //endTime
+
+                string curPosHar = opFirstPlay.posisiHari;
+
+                gameClockValue = opFirstPlay.posisiHari + " " + string.Format("{0}:{1}:{2}", gameClockTime.Hour, gameClockTime.Minute, gameClockTime.Second);
+                while (true)
+                {
+                    yield return null;
+                    gameClockTime = gameClockTime.AddSeconds(1 * gameClockSpeed);
+                    //proses posHar
+                    posHariInGameClock = getEndPosHar(curPosHar);
+
+                    gameClockValue = posHariInGameClock + " " + gameClockTime.ToString(" HH:mm:ss");//("dd/MM/yyyy HH:mm:ss");
+                    //+ "\nmulai: " + sTim.ToString("dd/MM/yyyy HH:mm") + "\ntamat: " + eTim.ToString("dd/MM/yyyy HH:mm");
+
+                    if (!MenuUnit.showPlayMode) { break; }
+
+                }//endwhile
+            }//endif
+        }//endwhile
+    }
+
+    private string getEndPosHar(string curPosHar)
+    {
+        string hPosHar = curPosHar.Substring(2).Replace(" ", ""); //cari string posisi hari bagian angkanya
+        //Debug.Log("hposHar: " + hPosHar);
+        int posHarInt = 0; //siap2 konversi string jadi int
+        Int32.TryParse(hPosHar, out posHarInt); //kalo gagal berarti hari H
+
+        int hEnd = posHarInt - (opFirstPlayStartDate.Date - gameClockTime.Date).Days;
+
+        //Debug.Log("selisih hari: "+(lastST - gameTime).Days+" lastST: "+lastST + " hEnd: " + hEnd);
+        string hEndStr = "Hari H";
+        if (hEnd < 0) { hEndStr = "H" + hEnd; }
+        else if (hEnd > 0) { hEndStr = "H+" + hEnd; }
+
+        return hEndStr;
+    }
+
+    private string preProcPosHar(string posHar)
+    {
+        string hPosHar = posHar.Substring(2).Replace(" ", "");
+        int posHarInt = 0;
+        Int32.TryParse(hPosHar, out posHarInt);
+
+        string hEndStr = "Hari H";
+        if (posHarInt < 0) { hEndStr = "H" + posHarInt; }
+        else if (posHarInt > 0) { hEndStr = "H+" + posHarInt; }
+
+        return hEndStr;
+    }
+
+    private IEnumerator playIndividualOperation(OperationItem opItIndv)
+    {
+        DateTime sT = opItIndv.startTime; //timeToIncrement
+        DateTime sTim = opItIndv.startTime;//startTime
+        DateTime eTim = opItIndv.startTime.Add(opItIndv.duration); //endTime
+        string kegClockValue = "";// opIt.posisiHari + " " + string.Format("{0}:{1}:{2}", sT.Hour, sT.Minute, sT.Second);
+        Debug.Log("start keg: " + opItIndv.name);
+        bool isStart = false;
+
+        while (true)
+        {
+            if (sT >= gameClockTime)
+            {
+                isStart = true;
+                eTim = opItIndv.startTime.Add(opItIndv.duration);
+            }
+            if (isStart)
+            {
+
+                sT = sT.AddSeconds(1 * gameClockSpeed);
+                kegClockValue = opItIndv.posisiHari + " " + sT.ToString("dd/MM/yyyy HH:mm")
+                    + "\nmulai: " + sTim.ToString("dd/MM/yyyy HH:mm") + "\ntamat: " + eTim.ToString("dd/MM/yyyy HH:mm");
+
+                //Debug.Log(kegClockValue);
+                if (sT >= eTim)
+                {
+                    Debug.Log("keg: " + opItIndv.name + " finished at " + sT.ToString());
+                    break;
+                }
+            }
+            yield return null;//new WaitForSeconds(0);
+        }
+        //Debug.Log("keg: " + opIt.name+" finished.");
+    }
+
+
+    //END PLAY MODE RELATED
 
     public static void setHariH(DateTime newHariH)
     {
